b3d83b18b98acd89350c5e96778ef689a4dc76d1
Daniel Mason <danmasson0@gmail.com>
2021-09-09 16:10:01 -0400

    added and refactored css to have Mealtime be more responsive going forward

--- components/views/About.js
+++ components/views/About.js
@@ -4,15 +4,25 @@ export default st => html`
   <main>
     <section id="about">
       <div class="about-header">
-        <h1>Mealtime</h1>
+        <h1>More About Mealtime</h1>
       </div>
-      <div class="about-text">
-        <h2></h2>
-        <p>
-          This is a creative endeavor aid a community of people manage the time
-          they spend in the kitchen with recipes that can fit specific
-          lifestyles
-        </p>
+      <div class="about-section">
+        <div class="about-text">
+          <h2>Background</h2>
+          <p>
+            This is a platform to aid a community of people to manage their time
+            efficiently as they spend time in the kitchen with recipes that can
+            fit specific lifestyles. We here a Mealtime are here to come
+            together with individuals out there in the wild world to free
+            themselves from their kitchens.
+          </p>
+          <p>
+            In the meantime, here at Mealtime we'd love to hear from you, to do
+            that head over to resources to let us hear from you, your thoughts,
+            suggestions, and dreams for Mealtime going forward. We'd love to
+            hear from you :).
+          </p>
+        </div>
       </div>
     </section>
   </main>

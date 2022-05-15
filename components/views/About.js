4a8a328a3543e33bb805feebcee27671d9a3dcf1
ahmedebid <ahmed-ebid@outlook.com>
2022-05-15 21:41:33 +0200

    Redesign the about page

--- components/views/About.js
+++ components/views/About.js
@@ -1,24 +1,21 @@
 import html from "html-literal";
 
 export default (st) => html`
-  <main>
+  <main id="about-page">
     <section id="about">
-      <div class="about-header">
-        <h1>More About Mealtime</h1>
+      <div class="about-image" id="about-1">
       </div>
-      <div class="about-section">
-        <div class="about-text">
-          <h2>Background</h2>
-          <p>
-            A platform that looks to aid people manage their time efficiently in their
-            kitchen with recipes that can suit different lifestyles.
-          </p>
-          <p>
-            In the meantime, we'd love to hear from you =)</br> To do that head over to
-            resources to let us hear from you, your thoughts, suggestions, and
-            dreams for Mealtime going forward.
-          </p>
-        </div>
+      <div class="about-text">
+        <h2>About MealTime</h2>
+        <p>We are a group of tech-savvy foodies! 😋👨‍💻👩‍💻</p>
+        <p>
+          We are building MealTime with a mission to help people manage their time efficiently in their kitchen with recipes that can suit different lifestyles. 🚀🚀
+        </p>
+        <p>
+          MealTime is a work-in-progress, so we'd love to hear from you 😀👋. Head over to Contact page and tell us your thoughts, suggestions, and dreams for MealTime going forward.
+        </p>
+      </div>
+      <div class="about-image" id="about-2">
       </div>
     </section>
   </main>

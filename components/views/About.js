7d769f3d3ca132f0e70ae542dfd3fec1b65881bf
ahmedebid <ahmed-ebid@outlook.com>
2022-05-22 16:12:33 +0200

    Finish redesigning the about page for mobile, tablet and desktop

--- components/views/About.js
+++ components/views/About.js
@@ -2,12 +2,12 @@ import html from "html-literal";
 
 export default (st) => html`
   <main id="about-page">
+    <h2>About MealTime</h2>
     <section id="about">
       <div class="about-image" id="about-1">
       </div>
       <div class="about-text">
-        <h2>About MealTime</h2>
-        <p>We are a group of tech-savvy foodies! 😋👨‍💻👩‍💻</p>
+        <h1>We are a group of tech-savvy foodies! 😋👨‍💻👩‍💻</h1>
         <p>
           We are building MealTime with a mission to help people manage their time efficiently in their kitchen with recipes that can suit different lifestyles. 🚀🚀
         </p>
